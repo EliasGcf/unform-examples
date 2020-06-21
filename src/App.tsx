72f7066fcd304026ee9c643149b97f840dad5f5f
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-06-21 18:39:34 -0300

    fix: add react-router-dom to fix the problem with github-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Form from './pages/Form';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <GlobalStyle />
       <Form />
-    </>
+    </BrowserRouter>
   );
 };
-
 export default App;
