e7df76627e502f8ef437b4bacf190da085a9f6a3
EliasGcf <eliasgabrielcf@gmail.com>
2020-06-21 22:53:11 +0000

    Deploying to gh-pages from  @ 828ee9edd722f66a580c2223beeecbbd34c76ca4 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92b9fc453fc76217159c4d27497b1b3f.js"
+  "/unform-select/precache-manifest.e0c8bda22331d7ef2ae0b7283e426030.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unform-select/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
