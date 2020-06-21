f08f8ad7365e53ccfa34803e4696d80aaa2fda6c
EliasGcf <eliasgabrielcf@gmail.com>
2020-06-21 22:00:05 +0000

    Deploying to gh-pages from  @ e9fb33ff27957303d93239902a07bf9fd2991321 🚀

---
+++ precache-manifest.5a693e762d34fcf502826e2eb0df466e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8e3cad113a0708ab2fee8f6bb4bf5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95eba0002e505a0c41b6",
+    "url": "/static/js/2.d5447789.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.d5447789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87fd50c50b72cdc51ca0",
+    "url": "/static/js/main.31b20be3.chunk.js"
+  },
+  {
+    "revision": "6ea73a08e5443f9f5008",
+    "url": "/static/js/runtime-main.2e87a93c.js"
+  }
+]);
